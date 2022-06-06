d14b7230786433f5117401d6b2177c5eb0dd8341
Philip King <king.phil@gmail.com>
2022-06-06 18:40:12 -0500

    'flux bootstrap' script

---
+++ scripts/flux-bootstrap.sh
@@ -0,0 +1,8 @@
+flux bootstrap gitlab \
+  --hostname=gitlab.pekware.xyz \
+  --ssh-hostname=gitlab.pekware.xyz \
+  --owner=maestro \
+  --repository=home-infra \
+  --branch=main \
+  --path=clusters/k3s-home \
+  --token-auth
