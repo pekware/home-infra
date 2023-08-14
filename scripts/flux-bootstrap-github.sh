ccf1f5c867d031721af8c58c35d9fb25bbff7973
Philip King <king.phil@gmail.com>
2023-08-14 16:41:17 -0500

    move directory w/secret keys

--- scripts/flux-bootstrap-github.sh
+++ scripts/flux-bootstrap-github.sh
@@ -1,5 +1,5 @@
 CONTEXT=${1:-staging}
-kubectl create -f $HOME/_scratch/_flux_bootstrap/$CONTEXT
+kubectl create -f $HOME/povault/sealed-secrets/$CONTEXT
 flux bootstrap github \
   --owner=kingphil \
   --repository=home-infra \
