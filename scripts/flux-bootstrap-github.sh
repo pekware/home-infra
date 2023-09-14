d6e7eef134694f50e79aa0dcaed8677af594ebaf
Philip King <king.phil@gmail.com>
2023-09-14 18:08:40 -0500

    add getopts to Flux CD bootstrap script to facilitate branches

--- scripts/flux-bootstrap-github.sh
+++ scripts/flux-bootstrap-github.sh
@@ -1,9 +1,21 @@
-CONTEXT=${1:-staging}
+CONTEXT=staging
+BRANCH=main
+
+while getopts c:b: name
+do
+  case $name in
+  c) CONTEXT="$OPTARG";;
+  b) BRANCH="$OPTARG";;
+  ?) printf "Usage: %s: [-c value] [-b value]\n" $0
+     exit 2;;
+  esac
+done
+
 kubectl create -f $HOME/povault/sealed-secrets/$CONTEXT
 flux bootstrap github \
   --owner=kingphil \
   --repository=home-infra \
-  --branch=main \
+  --branch=$BRANCH \
   --path=clusters/$CONTEXT \
   --private=true \
   --personal=true \
